593c26afbbe11c219776f84511b44c5ac9d6b244
Alexander <aleksandrharcenko@MacBook-Pro-Aleksandr.local>
2019-03-31 22:52:50 +0300

    upd

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ $(document).ready(function() {
         $('body,html').animate({scrollTop: top}, 1500);
     });
     
-    
+    $('input').blur(function() {
+        if($(this).val()) {
+            $(this).css('border-bottom', '2.5px solid #FED434');
+        } else {
+            $(this).css('border-bottom', '2.5px solid rgba(0,0,0,0.1)');
+        }
+    })
 })
